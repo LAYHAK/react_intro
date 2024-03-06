91263ee36ee3922901a804073fb4f32d5b79c7e5
LAYHAK <layhak.garu@gmail.com>
2024-03-06 19:51:18 +0700

    update: using map instead of write the product components 9 times

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import PageNavbar from './components/PageNavbar.jsx'
 import PageFooter from './components/PageFooter.jsx'
 import { Flowbite } from 'flowbite-react'
-import ProductCard from './components/ProductCard.jsx'
 import Feedback from './components/Feedback.jsx'
+import ProductCard from './components/ProductCard.jsx'
 
 function App() {
+    let nums = 9
     return (
         <Flowbite className="flex h-dvh flex-col justify-between ">
             <PageNavbar />
             <main className="container">
                 <section className=" my-10 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3  ">
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
+                    {/*loop product 9 times*/}
+                    {Array(nums)
+                        .fill()
+                        .map((_, i) => (
+                            <ProductCard key={i} />
+                        ))}
                 </section>
                 <Feedback />
             </main>
